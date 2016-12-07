5c00ef67c069b8d1af920f3b4a8365801cd91576
William Favorite <wfavorite@tablespace.net>
2016-12-07 14:09:12 -0500

    Work done update

--- version.h
+++ version.h
@@ -19,6 +19,7 @@
                    - Added support for -M(onochrome).
                    - Fixed bug/omission of "most" and "full" stats mode.
                    - Added support for testing for required files.
+                   - Some code cleanup.
 
 */
 #define VERSION_STRING "0.5.0"
